7a1b2635c5698cd86b0e3a3d3ee1c79e29684a89
DieEneSchrodinger <thijsvanloon@hccnet.nl>
2026-02-01 20:06:40 +0100

    Extended entrypoint with additional info

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 
 EXIT_FLAG=false
 
+echo "Restic-Backup v0.1.0"
+echo "Documentation at: https://github.com/DieEneSchrodinger/Restic-Backup"
+echo ""
+
 if [ -z "${BACKUP_SCRIPT_PATH}" ]; then
     echo "BACKUP_SCRIPT_PATH not set, exiting container due to missing mandatory variable."
     EXIT_FLAG=true
@@ -15,6 +19,9 @@ fi
 if [[ "$EXIT_FLAG" == true ]]; then
     exit 0;
 fi
+
+echo "Backup path: ${BACKUP_SCRIPT_PATH}"
+echo "Backup schedule: ${CRON_SCHEDULE}"
 
 chmod a+x "${BACKUP_SCRIPT_PATH}" > /dev/null 2>&1
 if [[ $? -ne 0 ]]; then
